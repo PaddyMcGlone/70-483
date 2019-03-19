12db828170fe4d5bf087a5839865e4aa5294f491
Paddy McGlone <paddymcglone@gmail.com>
2019-03-19 20:21:17 +0000

    Adding the background thread example.

---
+++ ChapterOne/BackgroundThread/BackgroundThread/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+
+namespace BackgroundThread
+{
+    public class Program
+    {
+        public static void ThreadMethod()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("ThreadProc: {0}", i);
+                Thread.Sleep(1000);
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            var t = new Thread(new ThreadStart(ThreadMethod));
+            t.IsBackground = false;
+            t.Start();
+        }
+    }
+}
