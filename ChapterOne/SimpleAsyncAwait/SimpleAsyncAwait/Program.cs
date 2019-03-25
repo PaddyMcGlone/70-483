236816ac26b79d98ebd3c9fc32eb7bbeb345ee0b
Paddy McGlone <paddymcglone@gmail.com>
2019-03-25 20:35:51 +0000

    Adding the Async and Await simple example.

---
+++ ChapterOne/SimpleAsyncAwait/SimpleAsyncAwait/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleAsyncAwait
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var result = DownloadContent().Result;
+            Console.WriteLine(result);
+        }
+
+        public static async Task<string> DownloadContent()
+        {
+            using (System.Net.Http.HttpClient client = new System.Net.Http.HttpClient())
+            {
+                var result = await client.GetStringAsync("http://www.microsoft.com");
+                return result;
+            }
+        }
+    }
+}
