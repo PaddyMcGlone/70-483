84c3e43c4e9af0382b421df6f93cb716aa7db83f
Paddy McGlone <paddymcglone@gmail.com>
2019-03-26 19:52:10 +0000

    Adding the AggregateException.

---
+++ ChapterOne/AggregateExceptions/AggregateExceptions/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace AggregateExceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Enumerable.Range(0, 20);
+
+            try
+            {
+                var parallelResult = numbers.AsParallel().Where(i => IsEven(i));
+
+                parallelResult.ForAll(i => Console.WriteLine(i));
+            }
+            catch (AggregateException e)
+            {
+                Console.WriteLine("There was {0} exception ", e.InnerExceptions.Count());                
+            }
+        }
+
+        public static bool IsEven(int i)
+        {
+            if (i % 10 == 0) throw new ArgumentException("i");
+
+            return i % 2 == 0;
+        }
+    }
+}
