72799e2f34ac23e72b9ec7f20922f235d8ebe48c
Paddy McGlone <paddymcglone@gmail.com>
2019-03-26 20:20:56 +0000

    Adding concurrent bag.

---
+++ ChapterOne/ConcurrentBag/ConcurrentBag/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConcurrentBag
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConcurrentBag<int> bag = new ConcurrentBag<int>();
+
+            bag.Add(21);
+            bag.Add(42);
+
+            int result;
+
+            if (bag.TryTake(out result))
+                Console.WriteLine(result);
+
+            if (bag.TryPeek(out result))
+                Console.WriteLine("The next item : {0}", result);
+        }
+    }
+}
