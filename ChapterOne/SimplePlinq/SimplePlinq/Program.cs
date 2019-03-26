e06d6280e1723d2ec87f372b5366a9513090df95
Paddy McGlone <paddymcglone@gmail.com>
2019-03-26 19:37:04 +0000

    Adding the simple Plinq query.

---
+++ ChapterOne/SimplePlinq/SimplePlinq/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimplePlinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Enumerable.Range(0, 100000);
+
+            var paralellResult = numbers.AsParallel().AsOrdered().Where(i => i % 2 == 0).ToArray();
+
+            foreach (var item in paralellResult)
+                Console.WriteLine(item);
+        }
+    }
+}
